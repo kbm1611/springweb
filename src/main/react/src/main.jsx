074237aa1a99482429d77771094c3db52c93171e
kbm1611 <bm1611@naver.com>
2026-03-11 10:56:10 +0900

    day48 react

--- src/main/react/src/main.jsx
+++ src/main/react/src/main.jsx
@@ -2,6 +2,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-    <App />
+// createRoot(document.getElementById('root')).render(
+//     <App />
+// )
+
+/// chapter4 예제
+import Exam2 from './chapter5/Exam2.jsx'
+createRoot(document.querySelector('#root')).render(
+    <Exam2 />
 )
